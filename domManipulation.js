4d13cdba53667c67afbee9562668218d4c708a64
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-06-29 15:53:05 +0100

    Create JS file

---
+++ domManipulation.js
@@ -0,0 +1,8 @@
+const changingTheHeaders = () => {  //function to change h1 element
+    let headerElement = document.getElementsByTagName("h1")
+    headerElement.innerText
+}
+
+const changePageBackground = () => {
+    document.getElementsByTagName("body") //function to change page backgroung colour
+}
